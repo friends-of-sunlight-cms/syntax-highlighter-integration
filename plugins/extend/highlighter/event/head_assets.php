aa6d88286340782d504b498e6466d4536eb66037
jDanek <jirka.danek@jdanek.eu>
2023-09-23 10:30:42 +0200

    Fix condition

--- plugins/extend/highlighter/event/head_assets.php
+++ plugins/extend/highlighter/event/head_assets.php
@@ -6,16 +6,19 @@ use Sunlight\WebState;
 return function (array $args) {
     global $_index, $_page;
 
+    $config = $this->getConfig();
+
     if (
         (
             $_index->type === WebState::PAGE
             && isset(Page::TYPES[$_page['type']])
-            && $this->getConfig()['in_' . Page::TYPES[$_page['type']]]
+            && isset($config['in_' . Page::TYPES[$_page['type']]])
+            && $config['in_' . Page::TYPES[$_page['type']]]
         )
-        || ($_index->type === WebState::PLUGIN && $this->getConfig()['in_plugin'])
-        || ($_index->type === WebState::MODULE && $this->getConfig()['in_module'])
+        || ($_index->type === WebState::PLUGIN && $config['in_plugin'])
+        || ($_index->type === WebState::MODULE && $config['in_module'])
     ) {
-        $args['css'][] = $this->getAssetPath('public/styles/' . $this->getConfig()['style'] . '.css');
+        $args['css'][] = $this->getAssetPath('public/styles/' . $config['style'] . '.css');
         $args['js'][] = $this->getAssetPath('public/highlight.pack.js');
         $args['js_after'] .= "\n<script>$(document).ready(function(){ $('span.pre').each(function(i, block) {hljs.highlightBlock(block);});});</script>";
     }
