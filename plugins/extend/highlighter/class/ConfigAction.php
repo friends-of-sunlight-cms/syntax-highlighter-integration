9c732bbadc6b66ca7f074d283f2614c810f3cd9c
jDanek <jirka.danek@jdanek.eu>
2024-02-03 18:18:43 +0100

    [WIP] Update compatibility for SL 8.1

--- plugins/extend/highlighter/class/ConfigAction.php
+++ plugins/extend/highlighter/class/ConfigAction.php
@@ -39,37 +39,37 @@ class ConfigAction extends BaseConfigAction
             ],
             'in_section' => [
                 'label' => _lang('highlighter.in_section'),
-                'input' => '<input type="checkbox" name="config[in_section]" value="1"' . Form::activateCheckbox($config['in_section']) . '>',
+                'input' => '<input type="checkbox" name="config[in_section]" value="1"' . Form::loadCheckbox('config', $config['in_section'], 'in_section') . '>',
                 'type' => 'checkbox'
             ],
             'in_category' => [
                 'label' => _lang('highlighter.in_category'),
-                'input' => '<input type="checkbox" name="config[in_category]" value="1"' . Form::activateCheckbox($config['in_category']) . '>',
+                'input' => '<input type="checkbox" name="config[in_category]" value="1"' . Form::loadCheckbox('config', $config['in_category'], 'in_category') . '>',
                 'type' => 'checkbox'
             ],
             'in_book' => [
                 'label' => _lang('highlighter.in_book'),
-                'input' => '<input type="checkbox" name="config[in_book]" value="1"' . Form::activateCheckbox($config['in_book']) . '>',
+                'input' => '<input type="checkbox" name="config[in_book]" value="1"' . Form::loadCheckbox('config', $config['in_book'], 'in_book') . '>',
                 'type' => 'checkbox'
             ],
             'in_group' => [
                 'label' => _lang('highlighter.in_group'),
-                'input' => '<input type="checkbox" name="config[in_group]" value="1"' . Form::activateCheckbox($config['in_group']) . '>',
+                'input' => '<input type="checkbox" name="config[in_group]" value="1"' . Form::loadCheckbox('config', $config['in_group'], 'in_group') . '>',
                 'type' => 'checkbox'
             ],
             'in_forum' => [
                 'label' => _lang('highlighter.in_forum'),
-                'input' => '<input type="checkbox" name="config[in_forum]" value="1"' . Form::activateCheckbox($config['in_forum']) . '>',
+                'input' => '<input type="checkbox" name="config[in_forum]" value="1"' . Form::loadCheckbox('config', $config['in_forum'], 'in_forum') . '>',
                 'type' => 'checkbox'
             ],
             'in_plugin' => [
                 'label' => _lang('highlighter.in_plugin'),
-                'input' => '<input type="checkbox" name="config[in_plugin]" value="1"' . Form::activateCheckbox($config['in_plugin']) . '>',
+                'input' => '<input type="checkbox" name="config[in_plugin]" value="1"' . Form::loadCheckbox('config', $config['in_plugin'], 'in_plugin') . '>',
                 'type' => 'checkbox'
             ],
             'in_module' => [
                 'label' => _lang('highlighter.in_module'),
-                'input' => '<input type="checkbox" name="config[in_module]" value="1"' . Form::activateCheckbox($config['in_module']) . '>',
+                'input' => '<input type="checkbox" name="config[in_module]" value="1"' . Form::loadCheckbox('config', $config['in_module'], 'in_module') . '>',
                 'type' => 'checkbox'
             ],
         ];
