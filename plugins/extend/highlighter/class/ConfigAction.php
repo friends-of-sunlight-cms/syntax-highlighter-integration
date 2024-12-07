a04390f74353da86b8c5970cc012cb147f3f5df4
jDanek <jirka.danek@jdanek.eu>
2024-12-07 18:13:45 +0100

    Update version. Replace inputs, textareas and selects with system Form class methods

--- plugins/extend/highlighter/class/ConfigAction.php
+++ plugins/extend/highlighter/class/ConfigAction.php
@@ -34,54 +34,44 @@ class ConfigAction extends BaseConfigAction
         return [
             'style' => [
                 'label' => _lang('highlighter.style'),
-                'input' => $this->createSelect('style', $styles, $config['style']),
+                'input' => Form::select('config[style]', $styles, $config['style'], ['class' => 'inputsmall']),
                 'type' => 'text'
             ],
             'in_section' => [
                 'label' => _lang('highlighter.in_section'),
-                'input' => '<input type="checkbox" name="config[in_section]" value="1"' . Form::loadCheckbox('config', $config['in_section'], 'in_section') . '>',
+                'input' => Form::input('checkbox', 'config[in_section]', '1', ['checked' => Form::loadCheckbox('config', $config['in_section'], 'in_section')]),
                 'type' => 'checkbox'
             ],
             'in_category' => [
                 'label' => _lang('highlighter.in_category'),
-                'input' => '<input type="checkbox" name="config[in_category]" value="1"' . Form::loadCheckbox('config', $config['in_category'], 'in_category') . '>',
+                'input' => Form::input('checkbox', 'config[in_category]', '1', ['checked' => Form::loadCheckbox('config', $config['in_category'], 'in_category')]),
                 'type' => 'checkbox'
             ],
             'in_book' => [
                 'label' => _lang('highlighter.in_book'),
-                'input' => '<input type="checkbox" name="config[in_book]" value="1"' . Form::loadCheckbox('config', $config['in_book'], 'in_book') . '>',
+                'input' => Form::input('checkbox', 'config[in_book]', '1', ['checked' => Form::loadCheckbox('config', $config['in_book'], 'in_book')]),
                 'type' => 'checkbox'
             ],
             'in_group' => [
                 'label' => _lang('highlighter.in_group'),
-                'input' => '<input type="checkbox" name="config[in_group]" value="1"' . Form::loadCheckbox('config', $config['in_group'], 'in_group') . '>',
+                'input' => Form::input('checkbox', 'config[in_group]', '1', ['checked' => Form::loadCheckbox('config', $config['in_group'], 'in_group')]),
                 'type' => 'checkbox'
             ],
             'in_forum' => [
                 'label' => _lang('highlighter.in_forum'),
-                'input' => '<input type="checkbox" name="config[in_forum]" value="1"' . Form::loadCheckbox('config', $config['in_forum'], 'in_forum') . '>',
+                'input' => Form::input('checkbox', 'config[in_forum]', '1', ['checked' => Form::loadCheckbox('config', $config['in_forum'], 'in_forum')]),
                 'type' => 'checkbox'
             ],
             'in_plugin' => [
                 'label' => _lang('highlighter.in_plugin'),
-                'input' => '<input type="checkbox" name="config[in_plugin]" value="1"' . Form::loadCheckbox('config', $config['in_plugin'], 'in_plugin') . '>',
+                'input' => Form::input('checkbox', 'config[in_plugin]', '1', ['checked' => Form::loadCheckbox('config', $config['in_plugin'], 'in_plugin')]),
                 'type' => 'checkbox'
             ],
             'in_module' => [
                 'label' => _lang('highlighter.in_module'),
-                'input' => '<input type="checkbox" name="config[in_module]" value="1"' . Form::loadCheckbox('config', $config['in_module'], 'in_module') . '>',
+                'input' => Form::input('checkbox', 'config[in_module]', '1', ['checked' => Form::loadCheckbox('config', $config['in_module'], 'in_module')]),
                 'type' => 'checkbox'
             ],
         ];
-    }
-
-    private function createSelect(string $name, array $options, $default): string
-    {
-        $result = "<select name='config[" . $name . "]'>";
-        foreach ($options as $k => $v) {
-            $result .= "<option value='" . $v . "'" . ($default == $v ? " selected" : "") . ">" . $k . "</option>";
-        }
-        $result .= "</select>";
-        return $result;
     }
 }
